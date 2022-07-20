92a0e2fd2eefb8f7d9f8d5fa9539e6b3de7edea3
Lucas Rodrigues <rodrigues14set@hotmail.com>
2022-07-19 22:45:40 -0300

    Módulo de Migrações

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+using var db = new CursoEFCore.Data.ApplicationContext();
+
+// Para toda vez que o projeto rodar acontecer a migração. Não é recomendado
+//db.Database.Migrate();
+
+// Como validar se existe migrações pendentes - Inicio
+var existe = db.Database.GetPendingMigrations().Any();
+
+if (existe)
+{
+    // Tomada de decisão
+}
+// Como validar se existe migrações pendentes - Fim
+
+
 Console.WriteLine("Hello, World!");
