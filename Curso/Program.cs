aedde53d811e250612641fa35ad31f832abb2644
Lucas Rodrigues <rodrigues14set@hotmail.com>
2022-07-21 22:50:24 -0300

    Operações com entity, removendo, consultando, alterando

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -18,7 +18,12 @@ if (existe)
 // Como validar se existe migrações pendentes - Fim
 
 //InserindoDados();
-InserirDadosEmMassa();
+//InserirDadosEmMassa();
+//ConsultarDados();
+//CadastrarPedido();
+//ConsultarPedidos();
+//AtualizarDados();
+RemoverRegistro();
 
 static void InserirDadosEmMassa()
 {
@@ -89,4 +94,111 @@ static void InserindoDados()
 
     var registros = db.SaveChanges();
     Console.WriteLine($"Total registro(s): {registros}");
+}
+
+static void ConsultarDados()
+{
+    using var db = new CursoEFCore.Data.ApplicationContext();
+
+    //var consultaPorSintaxe = (from c in db.Clientes where c.Id > 0 select c).ToList();
+    var consultaPorMetodo = db.Clientes
+                                .Where(p => p.Id > 0)
+                                .OrderBy(p => p.Id)
+                                .ToList();
+
+    foreach (var cliente in consultaPorMetodo)
+    {
+        Console.WriteLine($"Consultando cliente: {cliente.Id}");
+        //db.Clientes.Find(cliente.Id);
+        db.Clientes.FirstOrDefault(p => p.Id == cliente.Id);
+    }
+}
+
+static void CadastrarPedido()
+{
+    using var db = new CursoEFCore.Data.ApplicationContext();
+
+    var cliente = db.Clientes.FirstOrDefault();
+    var produto = db.Produtos.FirstOrDefault();
+
+    var pedido = new Pedido
+    {
+        ClienteId = cliente.Id,
+        IniciadoEm = DateTime.Now,
+        FinalizadoEm = DateTime.Now,
+        Observacao = "Pedido Teste",
+        Status = StatusPedido.Analise,
+        TipoFrete = TipoFrete.SemFrete,
+        Itens = new List<PedidoItem>
+        {
+            new PedidoItem
+            {
+                ProdutoId = produto.Id,
+                Desconto = 0,
+                Quantidade = 1,
+                Valor = 10
+            }
+        }
+    };
+
+    db.Pedidos.Add(pedido);
+    db.SaveChanges();
+}
+
+static void ConsultarPedidos()
+{
+    using var db = new CursoEFCore.Data.ApplicationContext();
+    var pedidos = db.Pedidos
+                    .Include(p => p.Itens)
+                        .ThenInclude(p => p.Produto)
+                    .ToList();
+
+    Console.WriteLine(pedidos.Count);
+}
+
+static void AtualizarDados()
+{
+    // Método Generico para atualizar um dado - Inicio
+
+    //using var db = new CursoEFCore.Data.ApplicationContext();
+    //var cliente = db.Clientes.FirstOrDefault(p => p.Id == 4);
+    //db.SaveChanges();
+
+    // Método Generico para atualizar um dado - Fim
+
+    // Método desconectado - Inicio
+
+    using var db = new CursoEFCore.Data.ApplicationContext();
+    //var cliente = db.Clientes.Find(4);
+
+    var cliente = new Cliente
+    {
+        Id = 4
+    };
+    var clienteDesconhecido = new
+    {
+        Nome = "Cliente Desconhecido passo 3",
+        Telefone = "7966669999"
+    };
+
+    db.Attach(cliente);
+    db.Entry(cliente).CurrentValues.SetValues(clienteDesconhecido);
+    db.SaveChanges();
+
+    // Método desconectado - Fim
+}
+
+static void RemoverRegistro()
+{
+    using var db = new CursoEFCore.Data.ApplicationContext();
+    
+    // Localizando o registro para depois excluir
+    //var cliente = db.Clientes.Find(4);
+
+    var cliente = new Cliente { Id = 6 };
+
+    //db.Clientes.Remove(cliente);
+    //db.Remove(cliente);
+    db.Entry(cliente).State = EntityState.Deleted;
+    db.SaveChanges();
 }
